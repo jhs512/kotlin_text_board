f0f3f4a8d167fc0d922cd3aefa16515b21987367
jhs512 <jangka512@gmail.com>
2021-04-07 19:01:22 +0900

    article add

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,8 @@
 fun main(args: Array<String>) {
     println("== 텍스트 게시판 시작 ==")
 
+    var articleLastId = 0
+
     while (true) {
         print("명령어) ")
         val command = readLine()?.trim()
@@ -8,7 +10,22 @@ fun main(args: Array<String>) {
         if (command.isNullOrEmpty())
             continue
 
-        println("입력된 명령어 : ${command}")
+        if (command == "article add") {
+            val id = ++articleLastId
+            print("제목 : ")
+            val title = readLine()?.trim()
+            print("내용 : ")
+            val body = readLine()?.trim()
+
+            println("== 입력된 게시물 ==")
+            println(
+                """
+                번호 : ${id}
+                제목 : ${title}
+                내용 : ${body}
+            """.trimIndent()
+            )
+        }
     }
 
     println("== 텍스트 게시판 끝 ==")
