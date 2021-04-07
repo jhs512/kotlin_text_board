05cc12078b7388673b43b9989d3314dcbe9b9136
jhs512 <jangka512@gmail.com>
2021-04-07 19:29:17 +0900

    게시물 리스팅 꾸미기

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,25 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+var articleLastId = 0
+val articles: MutableList<Article> = mutableListOf()
+
+fun addArticle(title: String, body: String) {
+    val id = ++articleLastId
+    val newArticle = Article(id, Util.getNowDateStr(), Util.getNowDateStr(), title, body)
+    articles.add(newArticle)
+}
+
+fun makeTestData() {
+    for (i in 1..10) {
+        addArticle("제목 ${i}", "내용 ${i}")
+    }
+}
+
 fun main(args: Array<String>) {
     println("== 텍스트 게시판 시작 ==")
 
-    var articleLastId = 0
+    makeTestData()
 
     while (true) {
         print("명령어) ")
@@ -11,22 +29,30 @@ fun main(args: Array<String>) {
             continue
 
         if (command == "article add") {
-            val id = ++articleLastId
             print("제목 : ")
-            val title = readLine()?.trim()
+            val title = readLine()!!.trim()
             print("내용 : ")
-            val body = readLine()?.trim()
+            val body = readLine()!!.trim()
 
-            println("== 입력된 게시물 ==")
-            println(
-                """
-                번호 : ${id}
-                제목 : ${title}
-                내용 : ${body}
-            """.trimIndent()
-            )
+            addArticle(title, body)
+        } else if (command == "article list") {
+            println("번호 / ${"제목".padEnd(17, ' ')} / ${"수정".padEnd(17, ' ')} / 제목")
+            for (article in articles) {
+                println("${article.id.toString().padEnd(4, ' ')} / ${article.regDate} / ${article.updateDate} / ${article.title}")
+            }
         }
     }
 
     println("== 텍스트 게시판 끝 ==")
+}
+
+data class Article(val id: Int, val regDate: String, val updateDate: String, val title: String, val body: String) {
+}
+
+object Util {
+    fun getNowDateStr(): String {
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        return current.format(formatter)
+    }
 }
